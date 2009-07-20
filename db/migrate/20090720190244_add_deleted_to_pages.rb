f4802a2543bff30950c03a042c04cd5c839294a2
August Lilleaas <augustlilleaas@gmail.com>
2009-07-20 21:39:33 +0200

    Have to explicitly set the `deleted` column to false, it seems.

--- db/migrate/20090720190244_add_deleted_to_pages.rb
+++ db/migrate/20090720190244_add_deleted_to_pages.rb
@@ -1,6 +1,7 @@
 class AddDeletedToPages < ActiveRecord::Migration
   def self.up
     add_column :pages, :deleted, :boolean, :default => false
+    Page.update_all(:deleted => false)
   end
 
   def self.down
