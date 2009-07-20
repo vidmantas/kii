4223b147228806f6d698d17690547937d1d4424e
August Lilleaas <augustlilleaas@gmail.com>
2009-07-20 10:55:07 +0200

    Removing the old parser helpers (they were all moved to lib/kii/markup.rb a while ago)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,25 +2,6 @@ module ApplicationHelper
   def render_body(body)
     Kii::Markup.new(body, self).to_html
   end
-  
-  def preliminary_page_body_parser(body)
-    body.gsub(/\[\[([^\]\]\|]+)\|?([^\]\]]+)?\]\]/) {|link| inline_page_link($~[1], ($~[2] || $~[1])) }.gsub(/\n+/, "<br/>")
-  end
-  
-  def inline_page_link(link_text, permalink)
-    page = Page.find_by_permalink(permalink.to_permalink)
-    options = {}
-    
-    if page
-      options[:class] = "pagelink exists"
-      options[:href] = page.permalink
-    else
-      options[:class] = "pagelink void"
-      options[:href] = permalink.to_permalink
-    end
-    
-    content_tag(:a, link_text, options)
-  end
 
   def page_title(title)
     @page_title = title
