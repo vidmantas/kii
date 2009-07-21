a8eedcd176d5471a84d25abe9ab71f7912d77c1e
August Lilleaas <augustlilleaas@gmail.com>
2009-07-21 21:25:04 +0200

    @page, not page. Adding test coverage for that.

--- test/functional/revisions_controller_test.rb
+++ test/functional/revisions_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class RevisionsControllerTest < ActionController::TestCase
+  test "show" do
+    get :show, :page_id => pages(:sandbox).to_param, :id => revisions(:sandbox_b).to_param
+    assert_response :success
+  end
+  
   test "changes with previous revision" do
     get :changes, :page_id => pages(:sandbox).to_param, :id => revisions(:sandbox_b).to_param
     assert_response :success
