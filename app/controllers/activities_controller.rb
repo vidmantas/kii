2633c5d7785904a64e6884dbb47f261382233846
August Lilleaas <augustlilleaas@gmail.com>
2009-09-16 19:06:00 +0200

    Adding smart grouping on the activity log, based on the timespan of the visible entries.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,23 @@
 class ActivitiesController < ApplicationController
   def index
-    @revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user])
+    ungrouped_revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user])
+    distance = ungrouped_revisions.first.created_at - ungrouped_revisions.last.created_at
+    
+    # TODO: This is one of the few cases where meta-programming would
+    # actually clean things up quite a bit ;)
+    
+    if distance < 1.month
+      @revisions = ungrouped_revisions.group_by {|r| r.created_at.at_beginning_of_day }.map {|date, revisions|
+        ["day", date, revisions]
+      }
+    elsif distance < 1.year
+      @revisions = ungrouped_revisions.group_by {|r| r.created_at.at_beginning_of_month }.map {|date, revisions|
+        ["month", date, revisions]
+      }
+    else
+      @revisions = ungrouped_revisions.group_by {|r| r.created_at.at_beginning_of_year }.map {|date, revisions|
+        ["year", date, revisions]
+      }
+    end
   end
 end
