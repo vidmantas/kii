1bde7f036f01f360c1d1a7218bc11dc55498aa76
August Lilleaas <augustlilleaas@gmail.com>
2009-09-16 19:09:46 +0200

    Using actual months, not just time between entries.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,16 +1,17 @@
 class ActivitiesController < ApplicationController
   def index
     ungrouped_revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user])
-    distance = ungrouped_revisions.first.created_at - ungrouped_revisions.last.created_at
+    first = ungrouped_revisions.first.created_at
+    last = ungrouped_revisions.last.created_at
     
     # TODO: This is one of the few cases where meta-programming would
     # actually clean things up quite a bit ;)
     
-    if distance < 1.month
+    if first.day != last.day && first.month == last.month
       @revisions = ungrouped_revisions.group_by {|r| r.created_at.at_beginning_of_day }.map {|date, revisions|
         ["day", date, revisions]
       }
-    elsif distance < 1.year
+    elsif first.month != last.month
       @revisions = ungrouped_revisions.group_by {|r| r.created_at.at_beginning_of_month }.map {|date, revisions|
         ["month", date, revisions]
       }
