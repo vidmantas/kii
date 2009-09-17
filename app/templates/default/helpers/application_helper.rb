36f20cec17d85b538157b559ae62f536ed5f2d75
August Lilleaas <augustlilleaas@gmail.com>
2009-09-17 17:36:23 +0200

    Rolling our own time_ago_in_words, shorter_time_ago_in_words. "5h" instead of "5 hours".

--- app/templates/default/helpers/application_helper.rb
+++ app/templates/default/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   
   def clever_timestamp(time)
     if time.today?
-      "#{time_ago_in_words(time)} ago"
+      "#{shorter_time_ago_in_words(time)} ago"
     else
       datetimestamp(time)
     end
@@ -23,6 +23,21 @@ module ApplicationHelper
   # 23:15
   def timestamp(time)
     time.strftime("%H:%M")
+  end
+  
+  # 5s instead of "5 seconds"
+  def shorter_time_ago_in_words(time)
+    distance = Time.now.to_i - time.to_i
+    case distance
+    when 0..59
+      "#{distance}s"
+    when 60..3599
+      "#{distance / 60}m"
+    when 3600..86399
+      "#{distance / 60 / 60}h"
+    else
+      "#{distance / 60 / 60 / 24}d"
+    end
   end
   
   def template_script(script)
