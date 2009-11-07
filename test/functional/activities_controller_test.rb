2dc46bbc70a847050c11e0b31407113185456528
August Lilleaas <augustlilleaas@gmail.com>
2009-11-07 23:04:13 +0100

    Making the activities controller test up to date

--- test/functional/activities_controller_test.rb
+++ test/functional/activities_controller_test.rb
@@ -3,6 +3,18 @@ require 'test_helper'
 class ActivitiesControllerTest < ActionController::TestCase
   test "index" do
     get :index
+    assert_redirected_to revisions_activities_path
+  end
+  
+  test "revisions" do
+    get :revisions
     assert_response :success
+    assert_template "activities/revisions"
+  end
+  
+  test "discussions" do
+    get :discussions
+    assert_response :success
+    assert_template "activities/discussions"
   end
 end
