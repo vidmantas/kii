f46c8ba50547fe01658359c1e1ccc4bf0682ea0d
August Lilleaas <augustlilleaas@gmail.com>
2009-11-07 22:58:00 +0100

    Adding 'tabs' to activity log - got log for revisions and discussions now.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,13 @@
 class ActivitiesController < ApplicationController
   def index
+    redirect_to revisions_activities_path
+  end
+  
+  def revisions
     @revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user])
+  end
+  
+  def discussions
+    @discussions = Discussion.all(:include => [{:latest_discussion_entry => :user}, :page], :order => "updated_at DESC", :limit => 50)
   end
 end
