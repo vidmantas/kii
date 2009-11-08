dfb33b7707c5e25c5de8581d82dbf8f600b782d4
August Lilleaas <augustlilleaas@gmail.com>
2009-11-08 03:13:05 +0100

    Don't ask me why I had to do that.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -15,7 +15,7 @@ class ActivitiesController < ApplicationController
   
   # Using params[:id] because the RSS reader won't be logged in. Exposing the ID isn't a problem, hopefully.
   def others_revisions
-    @revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user], :conditions => ["user_id != ?", params[:id]])
+    @revisions = Revision.find(:all, :order => "created_at DESC", :limit => 50, :include => [:page, :user], :conditions => ["user_id != ? OR user_id IS ?", params[:id], nil])
     @rss = url_for(:format => "rss", :id => params[:id])
     
     respond_to do |format|
