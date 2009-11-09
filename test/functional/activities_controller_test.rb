63a052321400fc7d9c5ff9fc7780d0e8b6b733ae
August Lilleaas <augustlilleaas@gmail.com>
2009-11-09 02:23:48 +0100

    Testing and fixing RSS for activity logs.

--- test/functional/activities_controller_test.rb
+++ test/functional/activities_controller_test.rb
@@ -12,6 +12,24 @@ class ActivitiesControllerTest < ActionController::TestCase
     assert_template "activities/revisions"
   end
   
+  test "revisions rss" do
+    get :revisions, :format => "rss"
+    assert_response :success
+    assert_template "activities/revisions.rss.erb"
+  end
+  
+  test "others revisions" do
+    get :others_revisions, :id => users(:admin).id
+    assert_response :success
+    assert_template "activities/others_revisions"
+  end
+  
+  test "others revision rss" do
+    get :others_revisions, :id => users(:admin).id, :format => "rss"
+    assert_response :success
+    assert_template "activities/revisions.rss.erb"
+  end
+  
   test "discussions" do
     get :discussions
     assert_response :success
